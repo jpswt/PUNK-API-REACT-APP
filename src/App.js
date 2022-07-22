7eddd1a966563827c62a79cd6f2dd9fbaeb43a9f
jpswt <joel_swt@yahoo.com>
2022-07-22 09:52:57 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+import BeerCard from './components/BeerCard';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			Beers: [],
+		};
+	}
+
+	componentDidMount() {
+		axios.get('https://api.punkapi.com/v2/beers').then((res) => {
+			const Beers = res.data;
+			this.setState({ Beers });
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header"> Punk API Beers </header>
+				<body>
+					<ol>
+						{this.state.Beers.map((beer, index) => (
+							<BeerCard
+								key={index}
+								name={beer.name}
+								image_url={beer.image_url}
+								first_brewed={beer.first_brewed}
+								tagline={beer.tagline}
+								abv={beer.abv}
+								description={beer.description}
+							/>
+						))}
+					</ol>
+				</body>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/BeerCard.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import '../css/BeerCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as faHeartInactive } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartActive } from '@fortawesome/free-solid-svg-icons';
+
+const BeerCard = (props) => {
+	const heart = faHeartInactive;
+	const likedHeart = faHeartActive;
+	const heartInactive = <FontAwesomeIcon icon={heart} />;
+	const heartActive = <FontAwesomeIcon icon={likedHeart} />;
+	const { name, image_url, first_brewed, tagline, abv, description } = props;
+	const [like, setLike] = useState(true);
+	return (
+		<li>
+			<img src={image_url} alt="" />
+			<div>
+				<h3>
+					{name}
+					<span>{first_brewed}</span>
+				</h3>
+				<h4>{tagline}</h4>
+				<p>
+					<span class="abv">ABV: {abv}</span>
+					{description}
+				</p>
+			</div>
+			<p
+				class="like"
+				onClick={() => {
+					like ? setLike(false) : setLike(true);
+				}}
+			>
+				{like ? heartInactive : heartActive}
+			</p>
+		</li>
+	);
+};
+
+export default BeerCard;
